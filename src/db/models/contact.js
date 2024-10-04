fbe4b1dddd5c1811b6b5d86165270d8cb2ddf561
Andrii Serdiuk <bihhi2@gmail.com>
2024-10-04 16:05:43 +0300

    commit-3

--- src/db/models/contact.js
+++ src/db/models/contact.js
@@ -1,34 +1,29 @@
-import { Schema, model } from 'mongoose';
+import { Schema, model } from "mongoose";
 
-const contactSchema = new Schema(
-  {
+const contactShema = new Schema({
     name: {
-      type: String,
-      required: true,
+        type: String,
+        required: true,
     },
     phoneNumber: {
-      type: String,
-      required: true,
+        type: String,
+        required: true,
     },
     email: {
-      type: String,
-      required: false,
+        type: String,
+        required: true,
     },
     isFavourite: {
-      type: Boolean,
-      default: false,
-      require: true,
+        type: Boolean,
+        default: false,
     },
     contactType: {
-      type: String,
-      enum: ['work', 'home', 'personal'],
-      required: true,
-      default: 'personal',
+        type: String,
+        enum: ["work", "home", "personal"],
+        default: "personal",
+        required: true,
+
     },
-  },
-  { timestamps: true },
-);
+}, { timestamps: true })
 
-const ContactCollection = model('contact', contactSchema);
-
-export default ContactCollection;
+export const contactCollection = model("contact", contactShema)
