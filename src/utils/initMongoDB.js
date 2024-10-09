91f25462f3373096166f7b75e3a6e86ec3d720b3
Andrii Serdiuk <bihhi2@gmail.com>
2024-10-09 22:36:30 +0300

    commit-04.10-2

--- src/utils/initMongoDB.js
+++ src/utils/initMongoDB.js
@@ -6,12 +6,12 @@ export const initMongoDB = async () => {
   try {
     const user = env(MONGO_DB_VARS.MONGO_USER);
     const password = env(MONGO_DB_VARS.MONGO_PASSWORD);
-    const url = env(MONGO_DB_VARS.MONGO_URL);
     const db = env(MONGO_DB_VARS.MONGO_DB);
 
-    await mongoose.connect(
-      `mongodb+srv://${user}:${password}@${url}/${db}?retryWrites=true&w=majority&appName=Cluster0`,
-    );
+    // Формуємо правильний URL без зайвих частин
+    const url = `mongodb+srv://${user}:${password}@bihhi87.7q1o4.mongodb.net/${db}?retryWrites=true&w=majority`;
+
+    await mongoose.connect(url);
 
     console.log('MongoDB connection successfuly');
   } catch (error) {
