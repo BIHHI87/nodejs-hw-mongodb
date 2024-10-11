e4c8fae44496a48811757eb56cc55d08366d1476
Andrii Serdiuk <bihhi2@gmail.com>
2024-10-11 13:13:36 +0300

    commit-11.10-8

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,4 +1,11 @@
 import { Contact } from '../db/models/contact.js';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+console.log('Current directory:', __dirname); // Перевірка поточної директорії
 
 export const getAllContacts = async () => {
   const contacts = await Contact.find(); 
@@ -9,4 +16,3 @@ export const getContactById = async (contactId) => {
   const contact = await Contact.findById(contactId); 
   return contact;
 };
-
